c98b3b4d4ff4d2af197d2c9504d87ba8768ebc6c
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-11 20:58:21 +0300

    Task (Remove Duplicates from Sorted Array) complete!

--- src/main/java/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/array/RemoveDuplicatesFromSortedArray.java
@@ -2,7 +2,24 @@ package array;
 
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4}; // Input array
+
+        int k = new RemoveDuplicatesFromSortedArray().removeDuplicates(nums);
+
 
     }
 
+    public int removeDuplicates(int[] nums) {
+
+        int sourcePointer = 1;
+        int uniqueElement = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > uniqueElement) {
+                uniqueElement=nums[i];
+                nums[sourcePointer] = nums[i];
+                sourcePointer++;
+            }
+        }
+        return sourcePointer;
+    }
 }
