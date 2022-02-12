90e52999cc57fd249856427406e960668814651f
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-12 19:59:42 +0300

    Task (ContainsDuplicate) completed!

--- src/main/java/array/ContainsDuplicate.java
+++ src/main/java/array/ContainsDuplicate.java
@@ -1,12 +1,21 @@
 package array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     public static void main(String[] args) {
 
     }
 
     public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
 
-        return true;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        if (set.size() != nums.length) return true;
+        else return false;
     }
 }
