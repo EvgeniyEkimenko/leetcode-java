6462be8bfe590884dccad9db0c85cb9976f6d334
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-14 16:23:29 +0300

    Task (First Unique Character in a String) is done!

---
+++ src/main/java/array/FirstUniqueCharacterInAString.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacterInAString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int firstUniqChar(String s) {
+
+        char[] chArray = s.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < chArray.length; i++) {
+            if (map.containsKey(chArray[i])) {
+                map.put(chArray[i], 2);
+            } else {
+                map.put(chArray[i], 1);
+            }
+        }
+
+        for (int i = 0; i < chArray.length; i++) {
+            if (map.get(chArray[i]) == 1) return i;
+        }
+
+        return -1;
+    }
+}
