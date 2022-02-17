5d333fc57b8a54ac5da2145fc29055cafff9d46a
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-17 14:02:14 +0300

    Task (Delete Node In a LinkedList) is done!

---
+++ src/main/java/linkedlist/DeleteNodeInLinkedList.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+public class DeleteNodeInLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public void deleteNode(ListNode node) {
+
+        while(node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
+
+
+/* Optimal solution
+                node.val = node.next.val;
+                node.next = node.next.next;
+                */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
