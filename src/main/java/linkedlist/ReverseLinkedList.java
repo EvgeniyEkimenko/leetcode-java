87373a146002f2a20d840da005cb95c6493c036b
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-18 14:55:17 +0300

    Task (Reverse Linked List) is done!

---
+++ src/main/java/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,37 @@
+package linkedlist;
+
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        //1->2->3->4->5->null
+        //null<-5<-4<-3<-2<-1
+        ListNode currentNode = head;
+        ListNode previousNode = null;
+
+        while (currentNode!=null) {
+            ListNode nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        return previousNode;
+    }
+}
