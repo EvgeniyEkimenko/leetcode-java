9ae2a00263a2ed7d239ac9d910e02c7f8c49ba47
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-21 17:23:11 +0300

    Task (Validate Binary Search Tree) is done!

---
+++ src/main/java/trees/ValidateBinarySearchTree.java
@@ -0,0 +1,39 @@
+package trees;
+
+public class ValidateBinarySearchTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public  boolean isValidBST(TreeNode root) {
+        return validate(root , null , null);
+    }
+
+    public  boolean validate(TreeNode root , Integer max, Integer min) {
+        if (root==null) {
+            return true;
+        }
+        else if (max!= null && root.val >= max || min!=null && root.val <= min) {
+            return false;
+        }
+        else {
+            return validate(root.left , root.val , min) && validate(root.right , max, root.val);
+        }
+    }
+ }
